78be20c5d9d7a2f1a0e7d639088985507c27e961
sujan <sujanpoojary884@gmail.com>
2020-04-13 14:39:34 +0530

    event deriven and styling component done.!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+import './Person/Person.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+    otherState: 'some other value',
+  };
+
+  switchNameHandler = (newName) => {
+    console.log('This event', this);
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+
   render() {
-    return React.createElement(
-      'div',
-      null,
-      React.createElement('h1', { className: 'App' }, 'Hi, I am React App')
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={(e) => this.switchNameHandler('Maximilian.!!')}>
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'max.!')}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          change={this.nameChangedHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
