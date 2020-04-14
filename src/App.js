3834f8e44db76b96f303da062ffdd39a86fb32ba
sujan <sujanpoojary884@gmail.com>
2020-04-14 10:30:48 +0530

    adding dynamic class is done.!

--- src/App.js
+++ src/App.js
@@ -44,11 +44,12 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
-      border: '1px solid blue',
+      border: 'none',
       padding: '8px',
       cursor: 'pointer',
+      color: 'white',
     };
     let persons = null;
     if (this.state.showPerson) {
@@ -67,11 +68,21 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button style={style} onClick={this.togglePersonHandler}>
           Persons List
         </button>
