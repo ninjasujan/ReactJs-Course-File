7cb9e4ab5f3f226521a5eaac6161530597c88957
sujan <sujanpoojary884@gmail.com>
2020-04-21 22:52:10 +0530

    initial setup for ajax module done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/containers/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App appTitle="Employee List" />,
-  document.getElementById('root')
-);
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
