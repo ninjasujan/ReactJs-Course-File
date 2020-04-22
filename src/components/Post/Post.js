cee2044cbe9365f19378beb61bc98ca2a2a2a792
sujan <sujanpoojary884@gmail.com>
2020-04-22 10:27:15 +0530

    fetching post using get method done.!

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,12 +3,12 @@ import React from 'react';
 import './Post.css';
 
 const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
-        </div>
-    </article>
+  <article className="Post">
+    <h1>{props.title}</h1>
+    <div className="Info">
+      <div className="Author">Author</div>
+    </div>
+  </article>
 );
 
 export default post;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    const posts = axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        this.setState({
+          posts: response.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post title={post.title} key={post.id} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
