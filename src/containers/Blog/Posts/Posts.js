089b49e57989ae81159be175e71407a7fabb01f7
sujan <sujanpoojary884@gmail.com>
2020-04-24 11:47:07 +0530

    programetically routing

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
 
 class Posts extends Component {
   state = {
@@ -11,9 +10,11 @@ class Posts extends Component {
   };
 
   selectPostHandler = (id) => {
-    this.setState({
-      selectedPost: id,
-    });
+    // this.setState({
+    //   selectedPost: id,
+    // });
+    // this.props.history.push({ pathname: '/' + id });
+    this.props.history.push('/' + id);
   };
 
   componentDidMount() {
@@ -47,13 +48,14 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link to={'/' + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.selectPostHandler(post.id)}
-            />
-          </Link>
+          // <Link to={'/' + post.id} key={post.id}>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.selectPostHandler(post.id)}
+          />
+          // </Link>
         );
       });
     }
