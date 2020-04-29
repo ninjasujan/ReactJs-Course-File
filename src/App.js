cc472e110d5c04b8a02dca93510dc49bbcb0cf7e
sujan <sujanpoojary884@gmail.com>
2020-04-29 09:16:34 +0530

    initial setup for redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Blog from './containers/Blog/Blog';
+
+import Counter from './containers/Counter/Counter';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+       <Counter />
+      </div>
     );
   }
 }
