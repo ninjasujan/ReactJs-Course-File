e97d434b6c4cbd3fab36838f2ec16161a65f4a08
sujan <sujanpoojary884@gmail.com>
2020-04-30 17:24:07 +0530

    reducer and dispath setup for event

---
+++ src/store/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+  counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'INCREMENT') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type === 'ADD') {
+    return {
+      counter: state.counter + 5,
+    };
+  }
+  if (action.type === 'SUBSTRACT') {
+    return {
+      counter: state.counter - 5,
+    };
+  }
+  return state;
+};
+
+export default reducer;
